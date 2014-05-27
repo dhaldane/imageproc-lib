6e5f2268faa272f6f8559c4e53c67e0e8dd8cf59
Duncan Haldane <dhaldane@berkeley.edu>
2014-05-27 15:39:18 -0700

    Update setpoints

--- ams-enc.c
+++ ams-enc.c
@@ -73,8 +73,8 @@ EncObj encPos[NUM_ENC];
 
 #define AMS_ENC_ANGLE_REG   0xFE
 
-#define AMS_ENC_OFFSET_0 13571
-#define AMS_ENC_OFFSET_1 12467
+#define AMS_ENC_OFFSET_0 6447
+#define AMS_ENC_OFFSET_1 7789
 
 volatile unsigned char  state = AMS_ENC_IDLE;
 volatile unsigned char  encoder_number = 0;
